fbe3e65b14d9385c930e077bcca5ed938f289b36
Dayton Harris <daytonh@classiccars.com>
2018-04-27 11:40:18 -0700

    Added blank companies view

---
+++ Attempt3/ClientApp/components/app-root.vue
@@ -0,0 +1,34 @@
+<template lang="html" src="../templates/shared/layout.cshtml"></template>
+
+<script>
+    import test from '../store/test.js'
+    import adminlte from '../../node_modules/admin-lte/dist/js/adminlte.min.js'
+    import Vue from 'vue' 
+    import { routes } from '../routes'
+
+
+    // Vue.component('counter-example', CounterExample);
+    // Vue.component('fetch-data', FetchData);
+    // Vue.component('home-page', HomePage);
+    // Vue.component('helloworld', HelloWorld);
+
+    export default {
+        data() {
+            return {
+                routes
+            }
+        },
+        components: {
+
+        }
+    }
+    
+</script>
+
+<style>
+    @import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css';
+    @import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css';
+    @import 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/AdminLTE.min.css';
+    @import 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/skins/_all-skins.min.css';
+    @import 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+</style>

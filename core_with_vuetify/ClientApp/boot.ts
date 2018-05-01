142c3b68dc4b0fbc8057895cbc0f72ba70ade48f
Dayton Harris <daytonh@classiccars.com>
2018-05-01 09:22:09 -0700

    core_with_vuetify initial commit

---
+++ core_with_vuetify/ClientApp/boot.ts
@@ -0,0 +1,19 @@
+import './css/site.css';
+import './stylus/main.styl';
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+
+const routes = [
+    { path: '/', component: require('./components/home/home.vue.html') },
+    { path: '/counter', component: require('./components/counter/counter.vue.html') },
+    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
+];
+
+new Vue({
+    el: '#app-root',
+    router: new VueRouter({ mode: 'history', routes: routes }),
+    render: h => h(require('./components/app/app.vue.html'))
+});

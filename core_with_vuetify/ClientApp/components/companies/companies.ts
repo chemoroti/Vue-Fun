8459d895a484751ef759c4987069d54d12b94765
Dayton Harris <daytonh@classiccars.com>
2018-05-01 11:35:56 -0700

    set up for db connection

--- core_with_vuetify/ClientApp/components/companies/companies.ts
+++ core_with_vuetify/ClientApp/components/companies/companies.ts
@@ -2,20 +2,19 @@ import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
 interface Company {
-    Id: number,
-    Name: string
+    id: number,
+    name: string
 }
 
 @Component
 export default class CompaniesComponent extends Vue {
-    things: Company[] = [];
+    companies: Company[] = [];
 
     mounted() {
         fetch('apiv1/Api/TopCompanies')
             .then(response => response.json() as Promise<Company[]>)
             .then(data => {
-                console.log(data);
-                this.things = data;
+                this.companies = data;
             });
     }
 }
